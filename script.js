7875ac36698a72a580d3efc041a68d2c7c9d1b30
abinash393 <abinashpanda393@gmail.com>
2019-10-01 00:00:04 +0530

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+const express = require('express');
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+// app.use((req, res, next) => {
+//     req.body = {};
+//     next()
+// });
+
+app.get('/', (req, res) => {
+    console.log(req.body);
+    res.send('access');
+});
+
+app.listen(3003, () => {
+    console.log('Express server started at port "3003"')
+});
